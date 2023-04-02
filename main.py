43708cc4f47ad1ce1efdde926c4b952d5f78a1e6
Danil Matveev <106242632+matvxxv@users.noreply.github.com>
2023-04-02 21:03:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-import asyncio
-import time
 import os
-
-from typing import Union
-
-import aiohttp
-from bs4 import BeautifulSoup
-import aiofiles
+import time
 import tempfile
 import hashlib
+from typing import Union
+from bs4 import BeautifulSoup
+import asyncio
+import aiohttp
+import aiofiles
 
 
+async def get_git_files_block(session: aiohttp.ClientSession,
+                              url: str
+                              ) -> Union[list, str]:
+    '''
+    Function returns the HTML block with directories and files links
+    '''
 
-async def get_git_files_block(session: aiohttp.ClientSession, url:str) -> Union[list,str]:
     response = await session.get(url=url)
     soup = BeautifulSoup(await response.text(), 'lxml')
     files_block = soup.find_all("td", class_="four")
@@ -22,7 +25,14 @@ async def get_git_files_block(session: aiohttp.ClientSession, url:str) -> Union[
     return files_block
 
 
-async def collect_dirs(session: aiohttp.ClientSession, repo_url: str, directory_url: str = None, directories: list = None) -> list:
+async def collect_dirs(session: aiohttp.ClientSession, repo_url: str,
+                       directory_url: str = None, directories: list = None
+                       ) -> list:
+
+    '''
+    Function collects directories links from repository in a list (including the root of repository)
+    '''
+
     dirs = []
     directories = [repo_url] if directories is None else directories
     directory_url = repo_url if directory_url is None else directory_url
@@ -38,22 +48,25 @@ async def collect_dirs(session: aiohttp.ClientSession, repo_url: str, directory_
             if dir_link not in dirs:
                 dirs.append(dir_link)
 
-    if dirs != []:
+    if dirs:
         for dir_link in dirs:
             directories.append(dir_link)
 
-    for dir in dirs:
-        await collect_dirs(session=session, repo_url=repo_url, directory_url=dir, directories=directories)
+    for dir_link in dirs:
+        await collect_dirs(session=session, repo_url=repo_url, directory_url=dir_link, directories=directories)
 
     return directories
 
 
-
 async def get_urls(session: aiohttp.ClientSession, directories: list) -> list:
+    '''
+    Function collects files urls from all directories in a list
+    '''
+
     repo_url = directories[0]
     links_list = []
-    for dir in directories:
-        files_block = await get_git_files_block(session, dir)
+    for dir_link in directories:
+        files_block = await get_git_files_block(session, dir_link)
         for file in files_block:
 
             link = str(file).split("href=\"")[1].split('\" title')[0].split("src/")[-1]
@@ -65,16 +78,20 @@ async def get_urls(session: aiohttp.ClientSession, directories: list) -> list:
     return links_list
 
 
-
 async def create_folders(folder_name: str, directories: list) -> list:
+    '''
+    Function creates folders (the same as repository has) in temporary directory
+    and returns absolute paths of these folders
+    '''
+
     git_directory_name = directories[0].split('/')[-1]
     repository_root = f'{folder_name}\\{git_directory_name}'
     directory_paths = [repository_root]
     if not os.path.exists(repository_root):
         os.makedirs(repository_root)
     if len(directories) > 1:
-        for dir in directories[1:]:
-            dir_path = f'{repository_root}\\{dir.split("master/")[-1]}'
+        for dir_link in directories[1:]:
+            dir_path = f'{repository_root}\\{dir_link.split("master/")[-1]}'
             if not os.path.exists(dir_path):
                 os.makedirs(dir_path)
             directory_paths.append(dir_path)
@@ -83,9 +100,11 @@ async def create_folders(folder_name: str, directories: list) -> list:
 
 
 async def download_file_tasks(session: aiohttp.ClientSession, folder_name: str, directory_paths: list,
-                              link: str, pathes: list = None) -> None:
-
-    pathes = [] if pathes is None else pathes
+                              link: str
+                              ) -> None:
+    '''
+    Function downloads files from repository in the temporary directory
+    '''
 
     repo_root_name = directory_paths[0].split(f'{folder_name}\\')[-1].replace('\\', '/')
     git_user_link = link.split(repo_root_name)[0]
@@ -98,14 +117,17 @@ async def download_file_tasks(session: aiohttp.ClientSession, folder_name: str, 
             async with aiofiles.open(os.path.join(f'{folder_name}\\{relative_way_to_dir}', file_name), 'wb') as f:
                 async for data in response.content.iter_any():
                     await f.write(data)
-                    pathes.append(f'{folder_name}\\{relative_way_to_dir}\\{file_name}'.replace('/', '\\'))
+                    paths.append(f'{folder_name}\\{relative_way_to_dir}\\{file_name}'.replace('/', '\\'))
 
 
+def hashing(paths: list) -> None:
+    '''
+    Hashing (sha256) downloaded files.
+    '''
 
-def hashing(pathes: list) -> None:
-    if pathes:
+    if paths:
         print("SHA256: ")
-        for path in pathes:
+        for path in paths:
             with open(path, 'rb') as f:
                 content = f.read()
                 sha256 = hashlib.sha256()
@@ -115,28 +137,27 @@ def hashing(pathes: list) -> None:
         print('There are no files for hashing')
 
 
-
 async def main():
     from tqdm.asyncio import tqdm_asyncio
+
     temp_dir = tempfile.mkdtemp()
 
     first_url = "https://gitea.radium.group/radium/project-configuration"
-
-    second_url = "https://gitea.radium.group/radium/flatfilecms"
-    third_url = "https://gitea.radium.group/radium/userhash"
 
     async with aiohttp.ClientSession() as session:
         directories = await collect_dirs(session, repo_url=first_url)
         files = await get_urls(session=session, directories=directories)
         directory_paths = await create_folders(temp_dir, directories)
-        await tqdm_asyncio.gather(*[download_file_tasks(session, temp_dir, directory_paths, file, pathes) for file in files], total=len(files))
+
+        await tqdm_asyncio.gather(*[download_file_tasks(session, temp_dir, directory_paths, file)
+                                    for file in files], total=len(files))
 
 
 if __name__ == "__main__":
-    pathes = []
+    paths = []
     t1 = time.perf_counter()
     print("process started...")
     asyncio.get_event_loop().run_until_complete(main())
     t2 = time.perf_counter()
     print(f'Completed in {t2-t1} seconds.', end='\n\n')
-    hashing(pathes)
+    hashing(paths)
